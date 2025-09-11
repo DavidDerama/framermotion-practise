ca8ec5e104ef4a15076bb6df39c6da36ae1c86ce
DavidDerama <davidbrentderama@gmail.com>
2025-09-11 20:25:20 +0300

    added glow to step

--- practise-project/src/components/Step.tsx
+++ practise-project/src/components/Step.tsx
@@ -19,11 +19,28 @@ export default function Step({ step, currentStep, maxSteps }: StepperProps) {
   return (
     <div className="relative">
       <motion.div
-        className={cn(
-          "rounded-full border-gray-300 border-2 py-2 px-[14px] flex justify-center items-center",
-          status === "complete" && "bg-blue-600 px-[10px]",
-          (currentStep > maxSteps || status === "complete") && "py-[10px]"
-        )}
+        animate={status}
+        variants={{
+          active: {
+            scale: 1,
+            transition: {
+              delay: 0,
+              duration: 0.3,
+            },
+          },
+          complete: {
+            scale: 1.3,
+          },
+        }}
+        transition={{
+          duration: 0.6,
+          delay: 0.2,
+          type: "tween",
+          ease: "circOut",
+        }}
+        className="absolute inset-0 scale-125 bg-blue-200 rounded-full"
+      ></motion.div>
+      <motion.div
         initial={false}
         animate={status}
         transition={{ duration: 0.2 }}
@@ -44,13 +61,18 @@ export default function Step({ step, currentStep, maxSteps }: StepperProps) {
             borderColor: "#3b82f6",
           },
         }}
+        className={cn(
+          "relative rounded-full border-gray-300 border-2 py-2 px-[14px] flex justify-center items-center",
+          status === "complete" && "bg-blue-600 px-[10px]",
+          (currentStep > maxSteps || status === "complete") && "py-[10px]"
+        )}
       >
         {status === "complete" ? (
           <motion.path>
-            <Check className="text-white h-4" />
+            <Check className="h-4 text-white" />
           </motion.path>
         ) : (
-          <p className="font-bold text-sm">{step}</p>
+          <p className="text-sm font-bold">{step}</p>
         )}
       </motion.div>
     </div>
