1535ae40aed4412de55e44ea7019cf09d5c2c1a2
DavidDerama <davidbrentderama@gmail.com>
2025-09-21 11:04:39 +0300

    gestures completed

--- crash-course/src/components/Sections/Gestures.tsx
+++ crash-course/src/components/Sections/Gestures.tsx
@@ -1,20 +1,24 @@
-import { motion } from "motion/react";
+import { motion, MotionConfig } from "motion/react";
 
 export default function Gestures() {
   return (
     <section className="flex flex-col gap-4 min-h-[60vh]">
-      <motion.button
-        className="p-5 py-2 text-sm text-white bg-blue-700 rounded-lg hover:bg-blue-700"
-        layout
-      >
-        Click Me!
-      </motion.button>
-      <motion.button
-        className="p-5 py-2 text-sm text-white bg-blue-700 rounded-lg hover:bg-blue-700"
-        layout
-      >
-        Click Me!
-      </motion.button>
+      <MotionConfig transition={{ duration: 0.125, ease: "easeInOut" }}>
+        <motion.button
+          className="p-5 py-2 text-sm text-white bg-blue-700 rounded-lg hover:bg-blue-700"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95, rotate: "2.5deg" }}
+        >
+          Click Me!
+        </motion.button>
+        <motion.button
+          className="p-5 py-2 text-sm text-white bg-blue-700 rounded-lg hover:bg-blue-700"
+          whileHover={{ scale: 1.15 }}
+          whileTap={{ scale: 0.85, rotate: "-2.5deg" }}
+        >
+          Click Me!
+        </motion.button>
+      </MotionConfig>
     </section>
   );
 }
